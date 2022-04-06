a2f0bdae9f6190e92ac65fd4d55796863e72bfbb
Sabrina Colman <sabrina-rct@hotmail.com>
2022-04-06 20:53:29 -0300

    Se agrego metodo console.readkey

--- EjerciciosCSharp.1.LaConsola/Program.cs
+++ EjerciciosCSharp.1.LaConsola/Program.cs
@@ -1,9 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-
+//punto 1 y punto 2
 Console.WriteLine("Por favor ingrese un comentario");
 string ingresoDelUsuario= Console.ReadLine();
 Console.WriteLine("Usted a escrito" + " " + ingresoDelUsuario);
 //cree el repositorio siguiendo las instrucciones
 Console.WriteLine("Gracias por su colaboracion ");
-Console.WriteLine("Probando subir otro cambio a la cuenta de github");
+
+
+/*punto 3: 
+ Lea las teclas presionadas y finalice al presionar la tecla “X”
+ metodo console.readkey
+Obtiene la siguiente tecla de carácter o de función presionada por el usuario
+ */
+Console.Write("Presione <Enter> para finalizar");
+while (Console.ReadKey().Key != ConsoleKey.Enter) ;
