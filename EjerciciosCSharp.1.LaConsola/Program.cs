e4dbeb027183b886c3ae914a4ed447cc1140383a
Sabrina Colman <sabrina-rct@hotmail.com>
2022-04-06 20:29:28 -0300

    Agregar archivos de proyecto.

---
+++ EjerciciosCSharp.1.LaConsola/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Por favor ingrese un comentario");
+string ingresoDelUsuario= Console.ReadLine();
+Console.WriteLine("Usted a escrito" + " " + ingresoDelUsuario);
